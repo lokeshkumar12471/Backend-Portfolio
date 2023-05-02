7d1f7e44c23dada4d563a245ba31a5befd310028
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2023-05-02 21:42:20 +0530

    React-Backend-Portfolio Installed

---
+++ app/Http/Controllers/Register.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class Register extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone'=>'required',
+            'age'=>'required',
+            'address'=>'required',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['name'] =  $user->name;
+        $success['email'] =  $user->email;
+        $success['phone'] =  $user->phone;
+        $success['age'] =  $user->age;
+        $success['address'] =  $user->address;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+
+            return $this->sendResponse($success, 'User login successfully.');
+        }
+        else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+}
+}
